d72eb8e1ac2ac4f8ce8036b0c9148f763f44e19f
Ethan Law and Sara Jensen <ethan@>
2016-08-09 14:58:52 -0700

    include javascript functions that presently prepend types of tea under the unordered lists of tea questions.

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,23 @@ $(document).ready(function() {
     // $(".brewpic").toggle();
   });
 
+  $("p#smoky").click(function(){
+    $("ul").append("<li>You should try black tea.</li>");
+  });
+
+  $("p#fruity").click(function(){
+    $("ul").append("<li>You should try hibiscus tea.</li>");
+  });
+
+
+  $("p#grassy").click(function(){
+    $("ul").append("<li>You should try green tea.</li>");
+  });
 });
+
+
+
+//worksheet
+// $("p#fruity").children("li").first().click(function() {
+//   $("ul").append("<li>You should try hibiscus tea.</li>");
+// });
